f67058a6d0c8c807e56156899f2d50a0ca5bffa9
Andrew Morris <voltrevo@gmail.com>
2015-06-01 11:26:39 +1000

    Got socket.io running

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,5 +1,11 @@
 'use strict';
 
+var io = require('socket.io-client');
+
 window.addEventListener('load', function() {
   document.body.appendChild(document.createTextNode('Hello world!'));
+
+  var sock = io(window.location.origin);
+
+  sock.emit('msg', 'foobar');
 });
--- src/server.js
+++ src/server.js
@@ -1,13 +1,24 @@
 'use strict';
 
+//var config = require('configure');
+
 var express = require('express');
 var browserify = require('browserify-middleware');
+var http = require('http');
+var socketIo = require('socket.io');
 
 var app = express();
+var server = http.Server(app);
+var io = socketIo(server);
+
+server.listen(8080);
+
 var projectRoot = __dirname + '/..';
-
 app.use('/index.js', browserify(__dirname + '/frontend/index.js'));
-
 app.use(express.static(projectRoot + '/public'));
 
-app.listen(8080);
+io.on('connection', function(sock) {
+  sock.on('msg', function(data) {
+    console.log(data);
+  });
+});
